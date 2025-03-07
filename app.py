3165e569138cdb3cfb204cc1fa76cc4b4158aa75
HarshDeshmukh23 <harshsdeshmukh23@gmail.com>
2025-03-07 10:16:39 +0530

    edited code

--- app.py
+++ app.py
@@ -9,13 +9,22 @@ c_points = 0
 while True:
     user_choice = input("Enter rock, paper, or scissors (or 'q' to exit): ").lower()
     
-    if user_choice == 'q':
-        print("Final Score - You:", u_points, "Computer:", c_points)
-        break   
+    if user_choice == comp_choice:
+        print("It's a tie! 🤝🏻")
+    elif(user_choice == 'rock' and comp_choice == 'scissors'):
+        print("Congratulations! You Won!! 🎉")
+        u_points += 1
+    elif(user_choice == 'scissors' and comp_choice == 'paper'):
+        print("Congratulations! You Won!! 🎉")
+        u_points += 1
+    elif(user_choice == 'paper' and comp_choice == 'rock'):
+        print("Congratulations! You Won!! 🎉")
+        u_points += 1
+    else:
+        print("Oops! You Lose 😔")
+        c_points += 1
     
-    if user_choice not in ['rock', 'paper', 'scissors']:
-        print("Invalid choice, try again!")
-        continue
+    print(f"Score - You: {u_points}, Computer: {c_points}\n")
     
     option = ['rock', 'paper', 'scissors']
     comp_choice = random.choice(option)
